797c0f933b9e66c6a32f615b8b0a8e3f92c5a497
JacobWellner83 <jcwellner83@gmail>
2019-04-06 12:29:50 -0500

    functions.js CallBacks Steps 2 & 3 complete. functions.js complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,8 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-function consume (x, y, cb){
+
+function consume (x, y, cb) {
   return cb(x, y)
 }
 
@@ -16,10 +17,22 @@ function consume (x, y, cb){
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
+
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
